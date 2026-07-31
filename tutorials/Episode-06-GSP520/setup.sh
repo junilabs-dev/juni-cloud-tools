ef4bc5c86fc4257de2538bc0976fee228ae2f824
junilabs-dev <junilabs-dev@users.noreply.github.com>
2026-08-01 02:25:33 +0530

    ✨ Apply custom branding banner and rename GSP520 notebook

--- tutorials/Episode-06-GSP520/setup.sh
+++ tutorials/Episode-06-GSP520/setup.sh
@@ -5,11 +5,11 @@
 echo -e "\e[1;92m🚀 Starting GSP520 Automation... \e[0m"
 
 echo -e "\e[1;96m[INFO] Downloading the solved notebook from juni-cloud-tools...\e[0m"
-wget "https://raw.githubusercontent.com/junilabs-dev/juni-cloud-tools/main/tutorials/Episode-06-GSP520/inspect_rich_documents_w_gemini_multimodality_and_multimodal_rag.ipynb" -O inspect_rich_documents_w_gemini_multimodality_and_multimodal_rag.ipynb
+wget "https://raw.githubusercontent.com/junilabs-dev/juni-cloud-tools/main/tutorials/Episode-06-GSP520/juni-cloud-tools-gsp520.ipynb" -O juni-cloud-tools-gsp520.ipynb
 
 echo -e "\e[1;92m[SUCCESS] Notebook downloaded successfully!\e[0m"
 echo -e "\e[1;93m[ACTION REQUIRED]\e[0m"
-echo -e "\e[1;97m1. Open the file 'inspect_rich_documents_w_gemini_multimodality_and_multimodal_rag.ipynb' from the left panel in JupyterLab.\e[0m"
+echo -e "\e[1;97m1. Open the file 'juni-cloud-tools-gsp520.ipynb' from the left panel in JupyterLab.\e[0m"
 echo -e "\e[1;97m2. Go to Edit -> Clear All Outputs.\e[0m"
 echo -e "\e[1;97m3. Go to Run -> Run All Cells.\e[0m"
 echo -e "\e[1;97m4. Wait for all cells to finish executing (this may take a few minutes).\e[0m"
