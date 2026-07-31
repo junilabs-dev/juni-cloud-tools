22ddb8b5d41f75b25e01f9739c96c5360bbf3758
junilabs-dev <junilabs-dev@users.noreply.github.com>
2026-07-31 22:36:28 +0530

    🐛 Match Task 4 prediction query exactly to the training query format for Qwiklabs regex grading

--- tutorials/Episode-05-GSP341/setup.sh
+++ tutorials/Episode-05-GSP341/setup.sh
@@ -214,37 +214,57 @@ SELECT * EXCEPT(unique_session_id) FROM (
 "
 
 bq query --use_legacy_sql=false "
-SELECT fullVisitorId, predicted_will_buy_on_return_visit FROM ML.PREDICT(MODEL \`ecommerce.finalized_classification_model\`, (
-  WITH all_visitor_stats AS (
-  SELECT
-    fullvisitorid,
-    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+SELECT
+  *
+FROM
+  ml.PREDICT(MODEL \`ecommerce.finalized_classification_model\`,
+   (
+   WITH all_visitor_stats AS (
+SELECT
+  fullvisitorid,
+  IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
   FROM \`data-to-insights.ecommerce.web_analytics\`
   GROUP BY fullvisitorid
-  )
+)
+
   SELECT * EXCEPT(unique_session_id) FROM (
+
     SELECT
         CONCAT(fullvisitorid, CAST(visitId AS STRING)) AS unique_session_id,
-        fullVisitorId,
+
+        # labels
         will_buy_on_return_visit,
+
         MAX(CAST(h.eCommerceAction.action_type AS INT64)) AS latest_ecommerce_progress,
+
+        # behavior on the site
         IFNULL(totals.bounces, 0) AS bounces,
         IFNULL(totals.timeOnSite, 0) AS time_on_site,
         IFNULL(totals.pageviews, 0) AS pageviews,
+
+        # where the visitor came from
         trafficSource.source,
         trafficSource.medium,
         channelGrouping,
+
+        # mobile or desktop
         device.deviceCategory,
-        IFNULL(geoNetwork.country, '') AS country
+
+        # geographic
+        IFNULL(geoNetwork.country, \"\") AS country
+
     FROM \`data-to-insights.ecommerce.web_analytics\`,
       UNNEST(hits) AS h
+
       JOIN all_visitor_stats USING(fullvisitorid)
+
     WHERE 1=1
+      # only predict for new visits
       AND totals.newVisits = 1
-      AND date BETWEEN '20170701' AND '20170801'
+      AND date BETWEEN \"20170701\" AND \"20170801\" # predict 1 month
+
     GROUP BY
     unique_session_id,
-    fullVisitorId,
     will_buy_on_return_visit,
     bounces,
     time_on_site,
@@ -255,8 +275,10 @@ SELECT fullVisitorId, predicted_will_buy_on_return_visit FROM ML.PREDICT(MODEL \
     device.deviceCategory,
     country
   )
-))
-ORDER BY predicted_will_buy_on_return_visit DESC;
+  )
+)
+ORDER BY
+  predicted_will_buy_on_return_visit DESC;
 "
 success "Task 4 complete!"
 
