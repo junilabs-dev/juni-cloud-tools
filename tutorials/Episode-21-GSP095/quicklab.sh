f5325094f77e45ead47ea8619554d195b31641db
junilabs-dev <junilabsdev@gmail.com>
2026-08-19 17:58:39 +0530

    🐛 Fix early deletion of Test1/Test2 by adding a pause

--- tutorials/Episode-21-GSP095/quicklab.sh
+++ tutorials/Episode-21-GSP095/quicklab.sh
@@ -9,18 +9,10 @@ gcloud pubsub topics create myTopic
 gcloud pubsub topics create Test1
 gcloud pubsub topics create Test2
 
-print_info "🗑️ Deleting Test1 and Test2 topics..."
-gcloud pubsub topics delete Test1
-gcloud pubsub topics delete Test2
-
 print_info "🚀 Task 2: Creating Pub/Sub subscriptions..."
 gcloud pubsub subscriptions create --topic myTopic mySubscription
 gcloud pubsub subscriptions create --topic myTopic Test1
 gcloud pubsub subscriptions create --topic myTopic Test2
-
-print_info "🗑️ Deleting Test1 and Test2 subscriptions..."
-gcloud pubsub subscriptions delete Test1
-gcloud pubsub subscriptions delete Test2
 
 print_info "🧪 Task 3 & 4: Publishing and pulling messages..."
 gcloud pubsub topics publish myTopic --message "Hello"
@@ -35,6 +27,19 @@ gcloud pubsub topics publish myTopic --message "Publisher wonders if all message
 gcloud pubsub topics publish myTopic --message "Publisher will have to test to find out"
 
 gcloud pubsub subscriptions pull mySubscription --limit=3
+
+print_info "====================================================="
+print_info "🛑 STOP HERE! Go to Qwiklabs and click 'Check my progress'"
+print_info "for all the tasks. Ensure you get 100/100 points."
+print_info "====================================================="
+read -p "Press [Enter] after you have verified the points to finish the cleanup..."
+
+print_info "🗑️ Cleanup: Deleting Test1 and Test2 topics and subscriptions..."
+gcloud pubsub subscriptions delete Test1
+gcloud pubsub subscriptions delete Test2
+gcloud pubsub topics delete Test1
+gcloud pubsub topics delete Test2
+
 
 success "🎉 Lab GSP095 Setup Complete!"
 print_info "Go back to Qwiklabs and click all the 'Check my progress' buttons to get 100/100 points!"
